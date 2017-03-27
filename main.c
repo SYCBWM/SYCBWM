b71e250ff846ae8fe267cedbaac93e3b4444c8db
Luxed <cocodu13820@hotmail.fr>
2017-03-27 17:11:54 -0400

    Trying to draw a 150x150 window with XCB

--- main.c
+++ main.c
@@ -1,159 +1,57 @@
-#include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
-#include <string.h>
-#include <sys/time.h>
 #include <xcb/xcb.h>
-#include <X11/Xlib.h>
-#define NUM_NAMES 500
-/*
-    NOTE: For concision, we're going to be cheesy and use arrays where real code
-    would use points and memory allocation.s
-*/
-#ifndef __GNUC__
-char* strdup(const char* s) {
-    int n = strlen(s) + 1;
 
-    char *dup = malloc(n);
+int main () {
+    xcb_connection_t        *con;               // Connection
+    xcb_screen_t            *screen;            // Screen
+    int                     screen_nbr;         // Screen number
+    xcb_screen_iterator_t   iter;               // Screen iterator
+    xcb_window_t            win;                // Window
 
-    if(dup)
-        strcpy(dup, s);
+    printf("XCB tests:\n");
 
-    return dup;
-}
-#endif
+    // Opening a connection to the X server
+    printf("Opening connection\n");
+    con = xcb_connect(NULL, &screen_nbr);
 
-/*
-    return interval of time (uses time.h)
-*/
-double
-get_time (void) {
-    struct timeval timev;
-    gettimeofday(&timev, NULL);
-    return (double)timev.tv_sec + (((double)timev.tv_usec) / 1000000);
-}
-
-/*
-
-*/
-void
-useXlib (char **names,
-         Display *display ) {
-
-    Atom atoms[NUM_NAMES];
-    for (int i = 0; i < NUM_NAMES; ++i) {
-        atoms[i] = XInternAtom(display, names[i], 0);
-    }
-}
-
-/*
-Request all atoms at once.
-*/
-void
-useXlibProperly (char **names,
-                 Display *display ) {
-
-    Atom atoms[NUM_NAMES];
-    if(!XInternAtoms(display, names, NUM_NAMES, 0, atoms))
-        fprintf(stderr, "XInternAtoms failed\n");
-}
-
-/*
-
-*/
-void
-useXCBPoorly (char **names,
-              xcb_connection_t *connection ) {
-    xcb_atom_t              atoms[NUM_NAMES];
-    // in this bad use of xcb, we use the cookie immediately after posting the request with xcb_intern_atom
-    for (int i = 0; i < NUM_NAMES; ++i) {
-        /* make request */
-        xcb_intern_atom_cookie_t cookie = xcb_intern_atom (connection,
-                                                           0,
-                                                           strlen(names[i]),
-                                                           names[i] );
-        /* get response */
-        xcb_intern_atom_reply_t *reply = xcb_intern_atom_reply (connection,
-                                                                cookie,
-                                                                NULL ); // normally a pointer to receive error, but we'll just ignore error handling
-        if (reply) {
-            atoms[i] = reply->atom;
-            free (reply);
+    // Get the number of screens
+    iter = xcb_setup_roots_iterator(xcb_get_setup(con));
+    for (; iter.rem; --screen_nbr, xcb_screen_next(&iter)) {
+        if (screen_nbr == 0) {
+            screen = iter.data;
+            break;
         }
     }
-    // now we have our atoms (replies), but this is just a demo, so we do nothing with them
-}
 
-/*
-*/
-void
-useXCBProperly (char **names,
-                xcb_connection_t *connection ) {
-    xcb_atom_t               atoms[NUM_NAMES];
-    xcb_intern_atom_cookie_t    cookies[NUM_NAMES];
-    // in this good example, we make all our requests before checking for
-    // replies because it's best to queue requests when we have many at once
-    /* make requests */
-    for (int i = 0; i < NUM_NAMES; ++i) {
-        cookies[i] = xcb_intern_atom (connection,
-                                      0,
-                                      strlen (names[i]),
-                                      names[i] );
-    }
-    /* get responses */
-    for (int i = 0; i < NUM_NAMES; ++i) {
-        xcb_intern_atom_reply_t *reply = xcb_intern_atom_reply (connection,
-                                                                cookies[i],
-                                                                NULL ); // normally a pointer to receive errors, but we'll just ignore error handling
-        if (reply) {
-            atoms[i] = reply->atom;
-            free (reply);
-        }
-    }
-    // now we have our atoms (replies), but this is just a demo, so we do nothing with them
-}
+    printf("\nScreen %ld:\n", screen->root);
+    printf("%dx%d\n", screen->width_in_pixels, screen->height_in_pixels);
 
-int
-main () {
-    /* setup names for tests */
-    char (**names) = malloc(NUM_NAMES*sizeof(*names));
-    // init names to "NAME0", "NAME1", "NAME2" ... and so on
-    for (int i = 0; i < NUM_NAMES; ++i) {
-        char buf[100];
-        sprintf (buf, "NAME%d", i);
-        names[i] = strdup (buf);
-    }
+    // Ask for a window's id
+    win = xcb_generate_id(con);
 
-    /* do tests */
-    double start, XlibTime, XlibGoodTime, XCBBadTime, XCBGoodTime;
+    // Creating the window
+    xcb_create_window(con, // Connection
+                      XCB_COPY_FROM_PARENT, // Depth
+                      screen->root, // Parent window
+                      win, // Id
+                      100,100, // x,y
+                      150,150, // Size: w,h
+                      10, // Border width
+                      XCB_WINDOW_CLASS_INPUT_OUTPUT, // Class
+                      screen->root_visual, // Visual
+                      0, NULL);
 
-    /* test Xlib */
-    Display *display = XOpenDisplay (NULL);
-    start = get_time ();
-    useXlib (names, display);
-    XlibTime = get_time () - start;
-    start = get_time ();
-    useXlibProperly (names, display);
-    XlibGoodTime = get_time () - start;
-    XCloseDisplay (display);
+    // Map window on the screen
+    xcb_map_window(con, win);
 
-    /* test XCB */
-    xcb_connection_t *connection = xcb_connect (NULL, NULL);
-    start = get_time ();
-    useXCBPoorly (names, connection);
-    XCBBadTime = get_time () - start;
-    start = get_time ();
-    useXCBProperly (names, connection);
-    XCBGoodTime = get_time () - start;
-    xcb_disconnect (connection);
+    xcb_flush(con);
 
-    /* report times */
-    printf ("Bad Xlib time : %f\n", XlibTime);
-    printf ("Good Xlib time : %f\n", XlibGoodTime);
-    printf ("Bad xcb time : %f\n", XCBBadTime);
-    printf ("Good xcb time : %f\n", XCBGoodTime);
-    printf ("ratio of good xcb time to bad xcb time: %f\n", XCBGoodTime / XCBBadTime);
-    printf ("ratio of Xlib time to good xcb time: %f\n", XlibTime / XCBGoodTime);
-    printf ("ratio of good Xlib time to bad Xlib time: %f\n", XlibGoodTime / XlibTime);
+    pause(); // Ctrl-c to exit
+
+    // Closing the connection to the X server
+    printf("\nDisconnecting");
+    xcb_disconnect(con);
 
     return 0;
 }
