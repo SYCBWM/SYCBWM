c75455e98268db2baa113ff012dddf27049c8b6b
Luxed <cocodu13820@hotmail.fr>
2017-03-30 23:59:09 -0400

    nbrMonitors() returns the number of screens

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <xcb/xcb.h>
+#include <xcb/randr.h>
 #include <jmorecfg.h>
 #include <stdbool.h>
+
+int nbrMonitors (xcb_connection_t *con, xcb_window_t root) {
+    // Xrandr get monitors
+    uint8_t active = 0; // use ?
+    xcb_randr_get_monitors_cookie_t cookie = xcb_randr_get_monitors(con, root, active);
+    xcb_randr_get_monitors_reply_t *test = xcb_randr_get_monitors_reply(con, cookie, NULL);
+    // They all seem to be the same
+    //printf("Number of monitors : %d\n", test->nMonitors);
+    //printf("Number of monitors : %d\n", test->nOutputs);
+    return xcb_randr_get_monitors_monitors_length(test);
+}
 
 int main () {
     xcb_connection_t        *con;               // Connection
@@ -19,26 +31,29 @@ int main () {
     printf("XCB tests:\n");
 
     // Opening a connection to the X server
-    printf("Opening connection\n");
+    printf("Opening connection\n\n");
     con = xcb_connect(NULL, &screen_nbr);
     if (xcb_connection_has_error(con)) {
         printf("Error opening display");
         exit(1);
     }
 
-    printf("%d\n", xcb_setup_roots_iterator(xcb_get_setup(con)).rem);
-
+    // Always returns 1 for me ??
+    //printf("%d\n", xcb_setup_roots_iterator(xcb_get_setup(con)).rem);
     // Get the number of screens
-    iter = xcb_setup_roots_iterator(xcb_get_setup(con));
+    /*iter = xcb_setup_roots_iterator(xcb_get_setup(con));
     for (; iter.rem; --screen_nbr, xcb_screen_next(&iter)) {
         if (screen_nbr == 0) {
             screen = iter.data;
             break;
         }
-    }
+    }*/
 
-    printf("\nScreen %ld:\n", screen->root);
+    screen = xcb_setup_roots_iterator(xcb_get_setup(con)).data;
+    printf("Screen %d:\n", screen->root);
     printf("%dx%d\n", screen->width_in_pixels, screen->height_in_pixels);
+
+    printf("Number of monitors : %d\n", nbrMonitors(con, screen->root)); // Returns the real number of screens
 
     // Ask for a window's id
     win = xcb_generate_id(con);
@@ -67,14 +82,14 @@ int main () {
         switch(event->response_type) {
             case XCB_KEY_PRESS:
                 btnPressed = ((xcb_key_press_event_t*)event);
-                printf("Keycode: %d\n", btnPressed->detail);
+                printf("\nKeycode: %d\n", btnPressed->detail);
                 /* ESC */
                 if (btnPressed->detail == 9) { // Finish if escape is pressed
                     finished = true;
                 }
                 break;
             case XCB_BUTTON_PRESS:
-                printf("Button pressed: %u\n", ((xcb_button_press_event_t*)event)->detail);
+                printf("\nButton pressed: %u\n", ((xcb_button_press_event_t*)event)->detail);
                 printf("X-coordinate: %u\n", ((xcb_button_press_event_t*)event)->event_x);
                 printf("Y-coordinate: %u\n", ((xcb_button_press_event_t*)event)->event_y);
                 break;
